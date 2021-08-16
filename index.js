241bfe09fc1bead76517e8c896d2e3c252ca8eca
samrath2007 <samrath1324@gmail.com>
2021-08-16 10:37:54 +0530

    feat: added odd and even checker

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 function isEven(number) {
-    if(number === 1) return false;
-    else if(number === 2) return true;
-    else if(number === 3) return false;
-    else if(number === 4) return true;
-    else if(number === 5) return false;
-    else if(number === 6) return true;
-    else if(number === 7) return false;
-    else if(number === 8) return true;
-    else if(number === 9) return false;
-    else if(number === 10) return true;
+  if (number % 2 === 0) {
+    return true
+  }
+  return false
 }
 
 module.exports = isEven;
