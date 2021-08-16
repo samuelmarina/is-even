9b520d94a0a4d214bcfd6fdce6bb8caebfd07c1c
NrdyBhu1 <61649925+NrdyBhu1@users.noreply.github.com>
2021-08-16 10:37:40 +0530

    add few more

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ function isEven(number) {
     else if(number === 8) return true;
     else if(number === 9) return false;
     else if(number === 10) return true;
+    else if(number === 11) return false;
+    else if(number === 12) return true;
+    else if(number === 13) return false;
+    else if(number === 14) return true;
+    else if(number === 15) return false;
+    else if(number === 16) return true;
+    else if(number === 17) return false;
+    else if(number === 18) return true;
+    else if(number === 19) return false;
+    else if(number === 20) return true;
+    else if(number === 21) return false;
+    else if(number === 22) return true;
 }
 
 module.exports = isEven;
