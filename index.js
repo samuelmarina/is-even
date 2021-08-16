4ef20dfbd11a3ef09324ea43bab74e6f5d5cd557
Typh0n <75157529+Typh0n12@users.noreply.github.com>
2021-08-16 10:46:38 +0530

    Some shitty code
    nothing to see here brrrrr

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 function isEven(number) {
-    if(number === 1) return false;
-    else if(number === 2) return true;
-    else if(number === 3) return false;
-    else if(number === 4) return true;
-    else if(number === 5) return false;
-    else if(number === 6) return true;
-    else if(number === 7) return false;
-    else if(number === 8) return true;
-    else if(number === 9) return false;
-    else if(number === 10) return true;
+    if(number%2===0){
+        return true;
+    } else{
+        return false;
+    }
 }
 
 module.exports = isEven;
