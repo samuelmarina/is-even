1e4a9087be3cc1120af0873d3545b1352f82319e
Stefan Blandin <stefanblandin@yahoo.com>
2021-08-17 12:03:45 -0700

    Fix segmentation fault
    Though we generally prefer not to use switch cases, in this situation we want to use them because it saves processor power. Unfortunately there aren't any easier ways to do this.

---
+++ tests/test_function.js
@@ -0,0 +1,31 @@
+is_even = require("../index.js")
+
+class Test{
+	constructor(description,func,input, expected){
+		this.description = description
+		this.func = func
+		this.input = input
+		this.expected = expected
+	}
+	test(){
+		let results = this.func(this.input)
+
+		if (results !== this.expected){
+			return {status:false, message:this.description + " failing. Results: " + results + " Expected: " + this.expected}
+		}
+
+		return {status:true, message: this.description + " Passing"}
+	}
+}
+
+exports.make_test_is_even = function(value, expected){
+	descr = "Test for "+value
+	input = value
+	func = function(input){
+		return is_even(input)
+	}
+
+	let newTest = new Test(descr, func, input, expected)
+
+	return newTest
+}
