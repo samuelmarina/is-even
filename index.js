24c2c15a4790b3e4e1a5d6bebd663dcba3caba06
cremble-cloud <80387756+cremble-cloud@users.noreply.github.com>
2021-08-18 12:02:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+function isNumber(n) { 
+return !isNaN(parseFloat(n)) && !isNaN(n - 0) 
+}
+
+var wtn = require('./wtn.js')
+
+ module.exports.isEven = function(input) {
+   
+   if(isNumber(input) == true) {
+     return input % 2 == 0;
+   } else if(isNumber(input) == false){
+  var cv = wtn(input)
+  return cv % 2 == 0;
+  }
+}
+
+/* examples:
+console.log(isEven('one-hundred'))
+
+will return true
+console.log(isEven('34587'))
+will return false
+
+GoldenFox made this.
+*/
