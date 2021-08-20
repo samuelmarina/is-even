363b590b05a50a44090daa85f790d2d7b22185b8
Shubham.Prasad <70384931+Shu6hamPrasad@users.noreply.github.com>
2021-08-20 07:20:26 +0530

    Update C++MakesItEasy.cpp
    added support for "1", "2"... and so on....

--- C++MakesItEasy.cpp
+++ C++MakesItEasy.cpp
@@ -4,18 +4,21 @@ int main()
 {
     ofstream file("index.js", ios::app);
     file << "function isEven(number) {" << endl;
-    file << "if (number === 1 )return true;" << endl; 
-    cout << "enter the no. of conditions: " ;
+    file << "if (number == 1 )return true;" << endl;
+    cout << "enter the no. of conditions: ";
+    file << "else if (number == \"even\" || number == \"Even\" || number == \"eVen\" || number == \"evEn\" || number == \"eveN\" || number == \"EVen\" || number == \"EvEn\" || number == \"EveN\" || number == \"eVEn\" || number == \"eVeN\" || number == \"evEN\" || number == \"eVEN\" || number == \"EvEN\" || number == \"EVeN\" || number == \"EVEn\" || number == \"EVEN\") return true;";
     int n;
     cin >> n;
     string k;
-    for (int i = 0; i <= n; i++) {
+    for (int i = 2; i <= n; i++)
+    {
         if (i % 2 == 0)
             k = " )return true;";
         else
             k = " )return false;";
-        file << "else if (number ===" << i << k << endl;
+        file << endl << "\telse if (number ==" << i << k << endl;
     }
+
     file << "}" << endl;
     file.close();
     return 0;
